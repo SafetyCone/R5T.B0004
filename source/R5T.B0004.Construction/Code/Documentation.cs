7b24f0bb581d7e925cb6c3b0440eeaaa094ef585
davidcoats <david@getsafetycone.com>
2022-02-28 12:10:39 -0800

    Initial commit of source-stub.

---
+++ source/R5T.B0004.Construction/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.B0004.Construction
+{
+    /// <summary>
+    /// Construction project for Using directive block label and sort order extension method bases..
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
---
+++ source/R5T.B0004/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.B0004
+{
+    /// <summary>
+    /// Using directive block label and sort order extension method bases.
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
